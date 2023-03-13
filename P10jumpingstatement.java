15ffe0fee1e8d22bf31a6f3611c084255a0a32d1
Mohit <127320469+MohitDhabale@users.noreply.github.com>
2023-03-13 19:56:53 +0530

    Update P10jumpingstatement.java

--- P10jumpingstatement.java
+++ P10jumpingstatement.java
@@ -2,7 +2,7 @@ public class P10jumpingstatement {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             if (i == 3) {
-                continue; // skip the third iteration
+                continue;
             }
             System.out.println(i + " " + (6 - i));
         }

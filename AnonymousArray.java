81f21629dd10f9125a96acd2e30856076a24d689
Mohit <127320469+MohitDhabale@users.noreply.github.com>
2023-03-27 09:19:55 +0530

    Add files via upload

---
+++ AnonymousArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class AnonymousArray
+
+{
+
+public static void main(String args[]){
+    
+    System.out.println(new int[]{55,52,33,48,68, 26}.length);
+    System.out.println(new int[]{91,44,17,41,11}[3]);
+    
+
+}
+}
